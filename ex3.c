be47caa76252b4accebd9b1d140278060de6f4ea
2002luis <luis.m.sousa10@gmail.com>
2022-11-28 15:57:45 +0000

    Fds

--- ex3.c
+++ ex3.c
@@ -15,6 +15,15 @@
 #define true 1
 #define BUFFSIZE 80
 
+unsigned long int strToUnsignedLong(char* in){
+    unsigned long int out = 0;
+    for(int i = 0; in[i]!='\0'; i++){
+        out*=10;
+        out+=(in[i]-'0');
+    }
+    return out;
+}
+
 unsigned long int doStuff(unsigned long int in, float chance, float timer, int thisn){
     srand(time(NULL));
     //printf("A fazer coisas\n");
@@ -28,78 +37,10 @@ unsigned long int doStuff(unsigned long int in, float chance, float timer, int t
     return(in+1);
 }
 
-int makeChild(int i, int limit, float timer, float chance){
-    char temp[80], n1[30], n2[30];
-    char pipe1[80], pipe2[80];
-    FILE* rf;
-    FILE* wt;
-    int thisn = i;
-
-    unsigned long int out = 1, k = 0;
-    sprintf(n1,"%d",i);
-    sprintf(n2,"%d",i+1);
-    strcpy(temp, "pipe");
-    strcat(temp, n1);
-    strcat(temp, "to");
-    strcat(temp, n2);
-    strcat(temp, ".fifo");
-
-    strcpy(pipe1, temp);
-
-    if(i<limit-1){
-        sprintf(n1,"%d",i+1);
-        sprintf(n2,"%d",i+2);
-        strcpy(temp, "pipe");
-        strcat(temp, n1);
-        strcat(temp, "to");
-        strcat(temp, n2);
-        strcat(temp, ".fifo");
-    
-    
-        pid_t pide = fork();
-        if(pide<0){
-            printf("Fork error\n");
-            return -1;
-        }
-        else if(pide==0){
-            //Nada
-        }
-        else{
-            makeChild(i+1, limit, timer, chance);
-        }
-
-    }
-    else{
-        strcpy(temp,"pipento1.fifo");
-        k = 300;
-    }
-    
-    strcpy(pipe2, temp);
-
-    while(true){
-         
-        rf = fopen(pipe1,"r");
-        char num[80];
-        fread(num,sizeof(num),1,rf);
-        k = (unsigned long int) atof(num);
-        //printf("%lu meme %lu \n",k, out);
-        if((k+1)!=out){
-            out = doStuff(k, chance, timer, 1);
-            out = k+1;
-            //printf("%lu cur Out\n",out);
-            wt = fopen(pipe2,"w");
-            fprintf(wt,"%lu",out);
-            fclose(wt);
-            
-        }
-    }
-
-    return 0;
-}
-
 int main(int argc, char* args[]){
-    FILE* rf;
-    FILE* wt;
+    int rf;
+    int wt;
+    int thisN = 1;
     if(argc!=4){
         printf("Needs 3 arguments\n");
         return -1;
@@ -108,7 +49,7 @@ int main(int argc, char* args[]){
     int n = (int)atof(args[1]);
     float timer = atof(args[2]);
     float chance = atof(args[3]);
-    char thispipe[80];
+    char pipe1[80] = "pipento1",pipe2[80] = "pipe1to2";
     for(int i = 0; i < n-1; i++){
         char pipename[80], n1[30], n2[30];
         sprintf(n1,"%d",i+1);
@@ -117,57 +58,69 @@ int main(int argc, char* args[]){
         strcat(pipename, n1);
         strcat(pipename, "to");
         strcat(pipename, n2);
-        strcat(pipename, ".fifo");
-        mkfifo(pipename,0666);
-
-        wt = fopen(pipename,"w");
-        fprintf(wt,"%lu",(unsigned long int) 0);
-        fclose(wt);
+        mkfifo(pipename,0777);
     }
-    char pipento1[] = "pipento1.fifo";
-    mkfifo(pipento1,0666);
-    wt = fopen(pipento1,"w");
-    fprintf(wt,"%lu",(unsigned long int) 0);
-    fclose(wt);
+    mkfifo("pipento1",0777);
+    unsigned long int out = 1;
+    char buf[8] = "1";
     
-    unsigned long int out = 300;
-    
-
-
-    pid_t pide = fork();
-    if(pide<0){
-        printf("Fork error\n");
-        return -1;
-    }
-    else if(pide==0){
-        //out = doStuff(out,chance,time, n);
-    }
-    else{
-        makeChild(1, n, timer, chance);
-    }
-    
-    
-    
-
-    unsigned long int k = 0;
-    while(true){
-        
-        rf = fopen("pipento1.fifo","r");
-        char num[80];
-        fread(num,sizeof(num),1,rf);
-        k = (unsigned long int) atof(num);
-        //printf("%lu meme %lu \n",k, out);
-        if((k+1)!=out){
-            out = doStuff(k, chance, timer, 1);
-            //printf("%lu cur Out\n",out);
-            wt = fopen("pipe1to2.fifo","w");
-            fprintf(wt,"%lu",out);
-            fclose(wt);
-            out = k;
+    for(int i = 1; i < n; i++)
+    {
+        pid_t pide = fork();
+        if(pide<0){
+            printf("Fork error\n");
+            return -1;
         }
-        //fwrite(num,strlen(num)+1,1,wt);
-        sleep(2);
-        
+        else if(pide==0){
+            char n1[30], n2[30];
+            //CHILD
+            thisN = i+1;
+            sprintf(n1,"%d",thisN-1);
+            sprintf(n2,"%d",thisN);
+            strcpy(pipe1, "pipe");
+            strcat(pipe1, n1);
+            strcat(pipe1, "to");
+            strcat(pipe1, n2);
+            if(thisN==n){
+                strcpy(pipe2, "pipento1");
+                wt = open(pipe2,O_WRONLY);
+                if(write(wt, buf, 8)==-1){
+                    return -1;
+                }
+            }
+            else{
+                sprintf(n1,"%d",thisN);
+                sprintf(n2,"%d",thisN+1);
+                strcpy(pipe2, "pipe");
+                strcat(pipe2, n1);
+                strcat(pipe2, "to");
+                strcat(pipe2, n2);
+            }
+            i = n;
+        }
+        else{
+            //PARENT
+        }
+    }
+
+    
+    printf("Processo %d pipe1 %s \t pipe2 %s\n",thisN,pipe1,pipe2);
+
+    
+
+    while(true){
+        //printf("Processo %d entrou no loop épico\n",thisN);
+        rf = open(pipe1,O_RDONLY);
+        out = read(rf,buf,8);
+        out = strToUnsignedLong(buf);
+        //printf("Processo %d acabou de ler %lu\n",thisN,out);
+        out = doStuff(out,chance,timer,thisN);
+        //printf("Processo %d acabou de fazer coisas %lu\n",thisN,out);
+        wt = open(pipe2,O_WRONLY);
+        if(write(wt, &out, 8)==-1){
+            return -1;
+        }
+        //printf("Processo %d acabou de escrever\n",thisN);
     }
 
     return 0;
